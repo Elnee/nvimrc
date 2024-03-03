06c211e45530b001005900183fa4882ed2bc0fc1
Elnee <hsoftdima@gmail.com>
2024-03-03 22:21:57 +0200

    Make grep window in telescope wider

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,6 +5,9 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({
+        search = vim.fn.input("Grep > "),
+        layout_strategy="current_buffer"
+    });
 end)
 
